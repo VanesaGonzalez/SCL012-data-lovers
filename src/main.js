f63f570b62876c70eda6a86d28042294983228e1
vanesa <vanesa.gonzalez395@gmail.com>
2019-12-07 21:50:01 -0300

    subiendo filtros de clases

--- src/main.js
+++ src/main.js
@@ -10,17 +10,18 @@ const champions = document.getElementById('champions');
 const championsButton = document.getElementById('championsButton');
 
 function setView(e) {
+  e.preventDefault();
   if (e.target.id === 'championsButton') {
     home.style.display = 'none';
-    champions.style.display = 'inherit';
+    champions.style.display = 'block';
     championsButton.style.color = 'rgba(55, 225, 227, 0.5)';
     homeButton.style.color = '';
 
-    let innerHTML = '<select name="" id="selectClases" class="classSelect"><options>      <option value ="">Todas las clases</option>      <option value ="Assassin">Asesino</option>      <option value ="Fighter">Luchador</option>      <option value ="Mage">Mago</option>      <option value ="Support">Soporte</option>      <option value ="Tank">Tanque</option>    </options>  </select>  <button class="positionButton">Superior</button>  <button class="positionButton">Jungla</button>  <button class="positionButton">Medio</button>  <button class="positionButton">Inferior</button>  <button class="positionButton">Soporte</button>'
+    let innerHTML = '<div class="row"><select name="" id="selectClases" class="classSelect"><options>      <option value ="">Todas las clases</option>      <option value ="Assassin">Asesino</option>      <option value ="Fighter">Luchador</option>      <option value ="Mage">Mago</option>      <option value ="Support">Soporte</option>      <option value ="Tank">Tanque</option>    </options>  </select>  <button class="positionButton">Superior</button>  <button class="positionButton">Jungla</button>  <button class="positionButton">Medio</button>  <button class="positionButton">Inferior</button>  <button class="positionButton">Soporte</button></div>'
     innerHTML += '<div id="championsWraper" class="row">'
     championsData.map(data => {
       innerHTML += '<div class = "championBox margin-top">';
-      innerHTML += '<button class="imageButton center"><img class="imgRedonda" src = "' + data.img + '"/></button>';
+      innerHTML += '<button class="imageButton center"><img class="imgRedonda" src = "' + data.splash + '"/></button>';
       innerHTML += '<p class="name-title">' + data.name + '</p>';
       innerHTML += '<p class="name-title">' + data.title + '</p>';
       innerHTML += '</div>'; // div que cierra  
@@ -30,13 +31,11 @@ function setView(e) {
     
     const selectClases = document.getElementById('selectClases');
     selectClases.addEventListener('change', setFilter);
-
   } else {
     home.style.display = 'inherit';
     champions.style.display = 'none';
     homeButton.style.color = 'rgba(55, 225, 227, 0.5)';
     championsButton.style.color = '';
-
   }
 
 }
@@ -45,11 +44,11 @@ function setFilter(e) {
   const filter = e.target.value;
   championsData = showChampion(filter);
   document.getElementById('championsWraper').remove;
-  let innerHTML = '<select name="" id="selectClases" class="classSelect"><options>      <option value ="">Todas las clases</option>      <option value ="Assassin">Asesino</option>      <option value ="Fighter">Luchador</option>      <option value ="Mage">Mago</option>      <option value ="Support">Soporte</option>      <option value ="Tank">Tanque</option>    </options>  </select>  <button class="positionButton">Superior</button>  <button class="positionButton">Jungla</button>  <button class="positionButton">Medio</button>  <button class="positionButton">Inferior</button>  <button class="positionButton">Soporte</button>'
+  let innerHTML = '<div class="row"><select name="" id="selectClases" class="classSelect"><options>      <option value ="">Todas las clases</option>      <option value ="Assassin">Asesino</option>      <option value ="Fighter">Luchador</option>      <option value ="Mage">Mago</option>      <option value ="Support">Soporte</option>      <option value ="Tank">Tanque</option>    </options>  </select>  <button class="positionButton">Superior</button>  <button class="positionButton">Jungla</button>  <button class="positionButton">Medio</button>  <button class="positionButton">Inferior</button>  <button class="positionButton">Soporte</button></div>'
   innerHTML += '<div id="championsWraper" class="row">'
   championsData.map(data => {
     innerHTML += '<div class = "championBox margin-top">';
-    innerHTML += '<button class="imageButton center"><img class="imgRedonda" src = "' + data.img + '"/></button>';
+    innerHTML += '<button class="imageButton center"><img class="imgRedonda" src = "' + data.splash + '"/></button>';
     innerHTML += '<p class="name-title">' + data.name + '</p>';
     innerHTML += '<p class="name-title">' + data.title + '</p>';
     innerHTML += '</div>'; // div que cierra  
