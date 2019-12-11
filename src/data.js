12d293adfa83b5066fed4181833d73dc31f7dc56
Nicolas Roman <nicolas.roman@nidarbox.com>
2019-12-11 09:45:31 -0300

    cambioseslint

--- src/data.js
+++ src/data.js
@@ -1,31 +1,31 @@
-
 import LoL from './data/lol/lol.js';
+
 export const showChampion = (filter) => {
-  const champions = []
-  Object.keys(LoL.data).forEach( (item) => {
-    if(LoL.data[item].tags.some(tag => tag === filter) || filter === ''){
+  const champions = [];
+  Object.keys(LoL.data).forEach((item) => {
+    if (LoL.data[item].tags.some((tag) => tag === filter) || filter === '') {
       champions.push(LoL.data[item]);
     }
   });
   return champions;
-}
+};
 
 export const lineChampions = (filter) => {
-  const lines = []
-  Object.keys(LoL.data).forEach( (item) => {
-    if(LoL.data[item].lines.some( line => line === filter) || filter === ''){
+  const lines = [];
+  Object.keys(LoL.data).forEach((item) => {
+    if (LoL.data[item].lines.some((line) => line === filter) || filter === '') {
       lines.push(LoL.data[item]);
     }
   });
   return lines;
-}
+};
 
 export const findById = (id) => {
-  let champion = {}
-  Object.keys(LoL.data).forEach( (item) => {
-    if(LoL.data[item].key === id) {
+  let champion = {};
+  Object.keys(LoL.data).forEach((item) => {
+    if (LoL.data[item].key === id) {
       champion = LoL.data[item];
     }
   });
-  return champion;  
-}
+  return champion;
+};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 import { showChampion, lineChampions, findById } from './data.js';
 
-let championsData = showChampion(''); 
+let championsData = showChampion('');
 let lineData = lineChampions('');
 
 const home = document.getElementById('home');
@@ -9,24 +9,54 @@ const homeButton = document.getElementById('homeButton');
 const champions = document.getElementById('champions');
 const championsButton = document.getElementById('championsButton');
 const filters = document.getElementById('filters');
-const modal = document.querySelector(".modal");
+const modal = document.querySelector('.modal');
 
 function makeChampions(data) {
   let innerHTML = '<div id="championsWraper" class="row">'; //
-  data.map(data => { //hace la caja de campeones
-    innerHTML += '<div class="margin-box"><button id="' + data.key + '" class="imageButton"><div id="' + data.key + '" class="championBox margin-top">'; // un div fuera del botón para generar el margen.Y el botón que engloba toda la caja q ue sea clickeable.
-    innerHTML += '<img id="' + data.key + '" class="imgRedonda center" src="' + data.splash + '"/>';
-    innerHTML += '<p id="' + data.key + '" class="name-title">' + data.name + '</p>';
-    innerHTML += '<p id="' + data.key + '" class="name-title">' + data.title + '</p>';
-    innerHTML += '</div></button></div>'; // div que cierra  
-
-  })
-  innerHTML += '</div>'
+  data.map((d) => { // hace la caja de campeones
+    innerHTML += `<div class="margin-box"><button id="${d.key}" class="imageButton"><div id="${d.key}" class="championBox margin-top">`; // un div fuera del botón para generar el margen.Y el botón que engloba toda la caja q ue sea clickeable.
+    innerHTML += `<img id="${d.key}" class="imgRedonda center" src="${d.splash}"/>`;
+    innerHTML += `<p id="${d.key}" class="name-title">${d.name}</p>`;
+    innerHTML += `<p id="${d.key}" class="name-title">${d.title}</p>`;
+    innerHTML += '</div></button></div>'; // div que cierra
+    return false;
+  });
+  innerHTML += '</div>';
 
   return innerHTML;
 }
 
-function makeButtons () { // captura los botones y agrega evento
+function setFilter(e) { // filtro en select o clases
+  const filter = e.target.value;
+  championsData = showChampion(filter); // filtra la información
+  document.getElementById('championsWraper').remove(); // borra todas las cajas
+  champions.innerHTML = makeChampions(championsData); // "dibuja" cajas con información filtrada
+
+  const imagesButtons = document.querySelectorAll('.imageButton');
+
+  imagesButtons.forEach((elem) => {
+    elem.addEventListener('click', openModal);
+  });
+
+  makeButtons();
+}
+
+function setFilterLine(e) {
+  const filter = e.target.id;
+  lineData = lineChampions(filter);
+  document.getElementById('championsWraper').remove();
+  champions.innerHTML = makeChampions(lineData);
+
+  const imagesButtons = document.querySelectorAll('.imageButton');
+
+  imagesButtons.forEach((elem) => {
+    elem.addEventListener('click', openModal);
+  });
+
+  makeButtons();
+}
+
+function makeButtons() { // captura los botones y agrega evento
   const selectClases = document.getElementById('selectClases');
   const topLine = document.getElementById('Top');
   const jgLine = document.getElementById('Jungle');
@@ -52,94 +82,67 @@ function setView(e) { // cambia entre inicio y campeones
 
     champions.innerHTML = makeChampions(championsData); // hace cada caja de campeones
 
-    const imagesButtons = document.querySelectorAll(".imageButton"); // botón de la caja, capturo y agrego evento
+    const imagesButtons = document.querySelectorAll('.imageButton'); // botón de la caja, capturo y agrego evento
 
-    imagesButtons.forEach(function(elem) {
-      elem.addEventListener("click", openModal);
+    imagesButtons.forEach((elem) => {
+      elem.addEventListener('click', openModal);
     });
 
-    makeButtons(); 
+    makeButtons();
   } else {
     home.style.display = 'inherit';
     champions.style.display = 'none';
+    filters.style.display = 'none';
     homeButton.style.color = 'rgba(55, 225, 227, 0.5)';
     championsButton.style.color = '';
   }
-
-}
-
-function setFilter(e) { //filtro en select o clases
-  const filter = e.target.value;
-  championsData = showChampion(filter); // filtra la información
-  document.getElementById('championsWraper').remove; //borra todas las cajas
-  champions.innerHTML = makeChampions(championsData); // "dibuja" cajas con información filtrada
-
-  const imagesButtons = document.querySelectorAll(".imageButton");
-
-  imagesButtons.forEach(function(elem) {
-    elem.addEventListener("click", openModal);
-  });
-
-  makeButtons();
-}
-
-function setFilterLine(e) {
-  const filter = e.target.id;
-  lineData = lineChampions(filter);
-  document.getElementById('championsWraper').remove;
-  champions.innerHTML = makeChampions(lineData);
-
-  const imagesButtons = document.querySelectorAll(".imageButton");
-
-  imagesButtons.forEach(function(elem) {
-    elem.addEventListener("click", openModal);
-  });
-  
-  makeButtons();
 }
 
 function openModal(e) {
-
-  const result = findById(e.target.id); //devuelve solo un campeón
+  const result = findById(e.target.id); // devuelve solo un campeón
 
   let tags = ''; // inicializo una variable para guardar tags, mapeo cada uno y cuando es el primero solo guardo el tag, y si vienen más le agrego guión
   result.tags.map((tag, index) => {
     if (index === 0) {
       tags = tag;
     } else {
-      tags += ' - ' + tag;
+      tags += ` - ${tag}`;
     }
+    return true;
   });
 
   let content = '<span class="close">&times;</span>'; // equis para cerrar modal
-  content += '<img class="modalImg" src="' + result.splash + '"/>'; //comienza a dibujar la caja
-  content += '<p class="modalTitle">' + tags + '</p>';
-  content += '<p id="blurb">' + result.blurb + '</p>';
-  content += '<p id="stats"> Life' + "   " + result.stats.hp + '</p>';
-  content += '<p id="stats"> Attack Range' + "   " + result.stats.attackrange + '</p>';
-  content += '<p id="stats"> Attack Speed Offset' + "   " + result.stats.attackspeedoffset + '</p>';
-  content += '<p id="stats"> Move Speed' + "   " + result.stats.movespeed + '</p>';
-  content += '<p id="stats"> Life Regen' + "   " + result.stats.hpregen + '</p>';
-  content += '<p id="stats"> Armor' + "   " + result.stats.armor + '</p>';
-  content += '<p id="stats"> Spell Block' + "   " + result.stats.spellblock + '</p>';
+  content += `<img class="modalImg" src="${result.splash}"/>`; // comienza a dibujar la caja
+  content += `<p class="modalTitle">${tags}</p>`;
+  content += `<p id="blurb">${result.blurb}</p>`;
+  content += '<br>';
+  content += '<div id="statsDiv"><div class="row">';
+  content += `<div class="row stat"><img src="../Images/Icons/vida.png"/><p id="stats"> Life: ${result.stats.hp}</p></div>`;
+  content += `<div class="row stat"><img src="../Images/Icons/regeneraciondevida.png"/><p id="stats"> Attack Range: ${result.stats.attackrange}</p></div>`;
+  content += `<div class="row stat"><img src="../Images/Icons/dañodeataque.png"/><p id="stats"> Attack Speed Offset: ${result.stats.attackspeedoffset}</p></div>`;
+  content += `<div class="row stat"><img src="../Images/Icons/armadura.png"/><p id="stats"> Move Speed: ${result.stats.movespeed}</p></div>`;
+  content += `<div class="row stat"><img src="../Images/Icons/velocidaddeataque.png"/><p id="stats"> Life Regen: ${result.stats.hpregen}</p></div>`;
+  content += `<div class="row stat"><img src="../Images/Icons/resistenciamagica.png"/><p id="stats"> Armor: ${result.stats.armor}</p></div>`;
+  content += `<div class="row stat lastStat"><img src="../Images/Icons/velocidaddemovimiento.png"/><p id="stats"> Spell Block: ${result.stats.spellblock}</p></div>`;
+  content += `</div></div>`;
 
 
-  modal.firstElementChild.innerHTML = content; 
-  modal.style.display = "block";
+  modal.firstElementChild.innerHTML = content;
+  modal.style.display = 'block';
 
-  const span = document.querySelector(".close");
+  const span = document.querySelector('.close');
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
+  span.onclick = function hide() {
+    modal.style.display = 'none';
+  };
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = function hide(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
   }
-}
+};
 
 homeButton.addEventListener('click', setView);
 championsButton.addEventListener('click', setView);
