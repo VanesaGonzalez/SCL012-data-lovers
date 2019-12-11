e24491d249ce8b42fa158ea2768ad17041ac13e0
vanesa <vanesa.gonzalez395@gmail.com>
2019-12-11 09:19:29 -0300

    solucionando errores de ESLint

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
-
 import LoL from './data/lol/lol.js';
+
 export const showChampion = (filter) => {
-  const champions = []
-  Object.keys(LoL.data).forEach( (item) => {
-    if(LoL.data[item].tags.some(tag => tag === filter) || filter === ''){
+  const champions = [];
+  Object.keys(LoL.data).forEach((item) => {
+    if (LoL.data[item].tags.some((tag) => tag === filter) || filter === '') {
       champions.push(LoL.data[item]);
     }
   });
   return champions;
-}
+};
 
 export const lineChampions = (filter) => {
-  const lines = []
-  Object.keys(LoL.data).forEach( (item) => {
-    if(LoL.data[item].lines.some( line => line === filter) || filter === ''){
+  const lines = [];
+  Object.keys(LoL.data).forEach((item) => {
+    if (LoL.data[item].lines.some((line) => line === filter) || filter === '') {
       lines.push(LoL.data[item]);
     }
   });
   return lines;
-}
+};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 import { showChampion, lineChampions } from './data.js';
 
-let championsData = showChampion(''); 
+let championsData = showChampion('');
 let lineData = lineChampions('');
 
 const home = document.getElementById('home');
@@ -9,7 +9,7 @@ const homeButton = document.getElementById('homeButton');
 const champions = document.getElementById('champions');
 const championsButton = document.getElementById('championsButton');
 
-function makeChampions(data) {
+function makeChampions(allChamp) {
   let innerHTML = `<div class='row'>
                     <select name='' id='selectClases' class='classSelect'>
                       <option value =''>Todas las clases</option>
@@ -23,32 +23,17 @@ function makeChampions(data) {
                     <button id='Jungle' class='positionButton'>Jungla</button>
                     <button id="Mid" class='positionButton'>Medio</button>
                     <button id="Bot" class='positionButton'>Inferior</button>
-                    <button id="Support" class='positionButton'>Soporte</button></div>`
-  innerHTML += '<div id="championsWraper" class="row">'
-  data.map(data => {
+                    <button id="Support" class='positionButton'>Soporte</button></div>`;
+  innerHTML += '<div id="championsWraper" class="row">';
+  allChamp.map(champion => {
     innerHTML += '<div class = "championBox margin-top">';
-    innerHTML += '<button class="imageButton center"><img class="imgRedonda" src = "' + data.splash + '"/></button>';
-    innerHTML += '<p class="name-title">' + data.name + '</p>';
-    innerHTML += '<p class="name-title">' + data.title + '</p>';
-    innerHTML += '</div>'; // div que cierra  
-  })
-  innerHTML += '</div>'
+    innerHTML += '<button class="imageButton center"><img class="imgRedonda" src = "' + champion.splash + '"/></button>';
+    innerHTML += '<p class="name-title">' + champion.name + '</p>';
+    innerHTML += '<p class="name-title">' + champion.title + '</p>';
+    innerHTML += '</div>';// div que cierra  
+  });
+  innerHTML += '</div>';
   return innerHTML;
-}
-
-function makeButtons () {
-  const selectClases = document.getElementById('selectClases');
-  const topLine = document.getElementById('Top');
-  const jgLine = document.getElementById('Jungle');
-  const midLine = document.getElementById('Mid');
-  const botLine = document.getElementById('Bot');
-  const supportLine = document.getElementById('Support');
-  selectClases.addEventListener('change', setFilter);
-  topLine.addEventListener('click', setFilterLine);
-  jgLine.addEventListener('click', setFilterLine);
-  midLine.addEventListener('click', setFilterLine);
-  botLine.addEventListener('click', setFilterLine);
-  supportLine.addEventListener('click', setFilterLine);
 }
 
 function setView(e) {
@@ -61,7 +46,7 @@ function setView(e) {
 
     champions.innerHTML = makeChampions(championsData);
 
-    makeButtons();
+    makeButtons();// eslint-disable-line no-use-before-define
   } else {
     home.style.display = 'inherit';
     champions.style.display = 'none';
@@ -77,7 +62,7 @@ function setFilter(e) {
   document.getElementById('championsWraper').remove;
   champions.innerHTML = makeChampions(championsData);
 
-  makeButtons();
+  makeButtons();// eslint-disable-line no-use-before-define
 }
 
 function setFilterLine(e) {
@@ -86,9 +71,23 @@ function setFilterLine(e) {
   document.getElementById('championsWraper').remove;
   champions.innerHTML = makeChampions(lineData);
   
-  makeButtons();
+  makeButtons();// eslint-disable-line no-use-before-define
+}
+
+function makeButtons() {
+  const selectClases = document.getElementById('selectClases');
+  const topLine = document.getElementById('Top');
+  const jgLine = document.getElementById('Jungle');
+  const midLine = document.getElementById('Mid');
+  const botLine = document.getElementById('Bot');
+  const supportLine = document.getElementById('Support');
+  selectClases.addEventListener('change', setFilter);
+  topLine.addEventListener('click', setFilterLine);
+  jgLine.addEventListener('click', setFilterLine);
+  midLine.addEventListener('click', setFilterLine);
+  botLine.addEventListener('click', setFilterLine);
+  supportLine.addEventListener('click', setFilterLine);
 }
 
 homeButton.addEventListener('click', setView);
 championsButton.addEventListener('click', setView);
-
